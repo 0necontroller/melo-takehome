1a103fc67fc19477d8de0a386caf03456cf21b99
Abel Misiocha <wells.mail.13@gmail.com>
2026-05-23 16:06:13 +0300

    fix(core): Build error

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,8 +1,8 @@
-import type { Role } from '@/lib/auth/roles';
+import type { UserRole } from '@/lib/auth/roles';
 
 export interface UserType {
 	id: string;
 	email: string;
 	name: string;
-	role: Role;
+	role: UserRole;
 }
